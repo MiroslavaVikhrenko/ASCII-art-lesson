2b117436e4d7dd65f6407761b9391054e6cf02d9
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-11-13 13:03:48 -0700

    Connect System.Windows.Forms for possibility to open images via open file dialog

--- ASCII art lesson/Program.cs
+++ ASCII art lesson/Program.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ASCII_art_lesson
 {
     internal class Program
     {
+        //In order we can convert an image into ASCII symbols - we need to get the original image first
+        //We can achieve this in different ways - it would be inconvenient to type the image path manually in console
+        //That's why in sonsole I connect winforms library and will use open file dialog to be able to open files via graphical interface
+        //We can do that even though the app will work from the console (Assemblies - Framework - System.Windows.Forms)
+
         static void Main(string[] args)
         {
         }
