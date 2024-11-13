e5d09a6cb5d9268dc8cc57232872e0552ad5c3f4
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-11-13 14:09:34 -0700

    Add resize image functionality to ensure the image fits the console

--- ASCII art lesson/Program.cs
+++ ASCII art lesson/Program.cs
@@ -5,11 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Drawing.Text;
 
 namespace ASCII_art_lesson
 {
     internal class Program
     {
+        //constant to ensure correct proportions are protected when we resize an image 
+        private const double WIDTH_OFFSET = 1.5;
+
         //In order we can convert an image into ASCII symbols - we need to get the original image first
         //We can achieve this in different ways - it would be inconvenient to type the image path manually in console
         //That's why in sonsole I connect winforms library and will use open file dialog to be able to open files via graphical interface
@@ -38,6 +42,7 @@ namespace ASCII_art_lesson
                 //until this method has worked - the code after that will not be executed
                 //method ReadLine() will work only after we enter anything into console or press enter
                 //so this way we will 'pause' the execution of the program
+
                 Console.ReadLine();
 
                 //code to convert an image into ASCII art
@@ -45,7 +50,8 @@ namespace ASCII_art_lesson
                 //ShowDialog() method result is represented with enum 'DialogResult'
                 //we are interested only in 'OK' as it's the only result that would lead to successful opening
                 //if it was not 'OK' then the later code won't execute and we will start in the beginning of the loop
-                if (openFileDialog.ShowDialog() != DialogResult.OK)  
+
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
                     continue;
 
                 //if we successfully opened the file, it means that we are going to open a new image 
@@ -64,6 +70,7 @@ namespace ASCII_art_lesson
                 //in the Bitmap() constructor we pass a path to the file that is stored in the property 'FileName' of the object of class OpenFileDialog 
                 //of course it will only be there IF we successfully opened the file - but unsuccessful scenarios are handled above in the if condition
                 //if we got so far => then it means that we already have an opened image and we can proceed with convertion
+
                 var bitmap = new Bitmap(openFileDialog.FileName);
 
                 //by its essense the image can be represented as a 2-dimensional array where each element is a pixel of a certain color
@@ -79,8 +86,38 @@ namespace ASCII_art_lesson
                 //simply put, font and fontsize settings decides the resolution of the image
                 //for picture to look nice we need a small font => that's why our top message Console.WriteLine("Press enter to start...\n"); might not be visible due to small font
                 //some fonts are brighter or more faded, wider or slimmer - it would also effect the way how the image is shown
+                //so far I set up the font size to 8 and marked as bold => will adjust further if needed
 
+                //---> #2 ORIGINAL IMAGE RESOLUTION
+                //we can open any image and the images we open can have very different resolutions
+                //theoretically we can face an issue when an opened image would not fit the console width
+                //to handle it we would need to add functionality to re-size/re-scale the image that we open to ensure it fits
+                
+
+                //make sure that the size fits - calling ResizedBitmap() method where the image will be resized IF needed
+                bitmap = ResizedBitmap(bitmap);
             }
+
+        }
+
+        //Method to re-size the image
+        private static Bitmap ResizedBitmap(Bitmap bitmap)
+        {
+            //set max image width 
+            var maxWidth = 350;
+            //we cannot change only image width otherwise the proportions will be distored
+            //so if we change the width, we also need to change height
+            //the below line ensures the proportions are kept the same
+            var newHeight = bitmap.Height / WIDTH_OFFSET * maxWidth / bitmap.Width;
+
+
+            //if the opened image by its width or height is bigger than the values we specified above, then we resize it
+            if (bitmap.Width > maxWidth || bitmap.Height > newHeight)
+                bitmap = new Bitmap(bitmap, new Size(maxWidth, (int)newHeight));
+
+            //if the opened image width and height are fine, we do nothing, just return it as it is
+
+            return bitmap;
         }
     }
 }
